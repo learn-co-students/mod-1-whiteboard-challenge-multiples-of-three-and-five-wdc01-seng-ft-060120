a1f94bfa6822c717da167009f6c2ccca2da57a8b
mjester93 <m.jester.93@gmail.com>
2020-06-17 09:32:25 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,9 +1,4 @@
 # Enter your procedural solution here!
-sum_of_nums_less_10_multiple_of_three_or_five = (1...10).select {|num| num % 3 == 0 || num % 5 == 0}.sum
-sum_of_nums_less_1000_multiple_of_three_or_five = (1...1000).select {|num| num % 3 == 0 || num % 5 == 0}.sum
-
-puts sum_of_nums_less_10_multiple_of_three_or_five
-puts sum_of_nums_less_1000_multiple_of_three_or_five
 
 def collect_multiples(limit)
     return (1...limit).select {|num| num % 3 == 0 || num % 5 == 0}
