d51142d0629dfb79cfb2d7e2e1ce51bc1e2b2ef0
Kosi Akporji <kosiazom@gmail.com>
2020-06-17 22:33:22 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+require 'pry'
+
+def collect_multiples(limit)
+    #find the multiples of number in an array
+    num = (1...limit).to_a
+   multiples = num.select do |num| 
+        num % 3 == 0 || num % 5 == 0
+      end
+   multiples
+end
+
+def sum_multiples(limit)
+   collect_multiples(limit).sum
+   #binding.pry
+end
